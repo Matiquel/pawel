c78de6c14ed5cd731f5eee44ce100a6092a73c79
Matiquel <Mateusz.pyl@interia.pl>
2018-01-15 16:29:15 +0100

    Update kluby.php

--- kluby.php
+++ kluby.php
@@ -2,69 +2,76 @@
 include("header.php");
 ?>
 
+  <style media="screen">
+  @media (min-width:992px) {
+  .center {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -webkit-justify-content: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  }
+  }
+  </style>
+
+<script type="text/javascript">
+  document.title = 'Baza Klubów | transferypilkarskie.pl';
+</script>
+
 <div class="card-body center">
 <div class="col-lg-6">
-    <div class="input-group">
-      <input type="text" class="form-control" placeholder="Wyszukaj klub..." aria-label="Search for...">
-      <span class="input-group-btn"  style="margin-left: 1rem">
+    <div class="input-group search-box">
+      <input type="text" class="form-control" placeholder="Wyszukiwarka jeszcze nie działa..." readonly  disabled aria-label="Search for...">
+      <!-- <span class="input-group-btn" style="margin-left: 1rem">
         <button class="btn btn-secondary" type="button">Szukaj</button>
-      </span>
+      </span> -->
     </div>
   </div>
   </div>
 </div>
 
 
+  <div class="row center" id="karty">
+    <?php
+    $zapytanie = $DB_con->query("SELECT * FROM klub");
 
+    foreach ($zapytanie as $wiersz) {
 
-  <div class="card-deck">
-      <div class="card" style="margin-top: 20px;">
+    ?>
+      <div class="card mr-c kluby" style="width: 20rem;margin-bottom: 5px;" id="<?php echo $wiersz['id']?>">
 
         <div class="card-body">
-          <div class="row">
+
            <div class="col-md-12 text-center">
-             <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Paris_Saint-Germain_F.C..svg/1200px-Paris_Saint-Germain_F.C..svg.png" class="img-responsive rounded mx-auto d-block" alt="png" style="width:84px; height:84px;">
+             <img src="<?php echo $wiersz['logo'];?>" class="img-responsive rounded mx-auto d-block" alt="png" style="width:84px; height:84px;">
       </div>
-      </div>
+
     </div>
     <div class="card-footer text-center">
-      <b>Paris Saint Germain</b>
+      <b><?php echo $wiersz['nazwa']?></b>
     </div>
     </div>
+    <?php
+
+  }
+  $zapytanie->closeCursor();
+    ?>
 
 
 
 
-  <div class="card" style="margin-top: 20px;">
-
-    <div class="card-body">
-      <div class="row">
-       <div class="col-md-12 text-center">
-         <img src="https://vignette.wikia.nocookie.net/logopedia/images/0/07/Fc_barcelona.png/revision/latest?cb=20120211172706&format=original" class="img-responsive rounded mx-auto d-block" alt="png" style="width:84px; height:84px;">
-  </div>
-  </div>
-</div>
-<div class="card-footer text-center">
-  <b>FC Barcelona</b>
-</div>
 </div>
 
+<script type="text/javascript">
+$('.kluby').on("click", function(){
 
-<div class="card" style="margin-top: 20px;">
-
-  <div class="card-body">
-    <div class="row">
-     <div class="col-md-12 text-center">
-       <img src="img/real.png" class="img-responsive rounded mx-auto d-block" alt="png" style="width:84px; height:84px;">
-     </div>
-   </div>
- </div>
-  <div class="card-footer text-center">
-    <b>Real Madryt</b>
-  </div>
-</div>
-
-</div>
+ //  console.log(this.id);
+   window.location.href = "klub-info.php?id="+this.id;
+});
+</script>
 
 
 <?php include("footer.php");?>
